abe46e5a1eec41b93fae840b1405a3eec3c319e8
saurabh5252 <113355821+saurabh5252@users.noreply.github.com>
2022-09-14 01:34:38 +0530

    Added PrimeNum Code

---
+++ PrimeNum.java
@@ -0,0 +1,25 @@
+package com.velocity.git;
+
+import java.util.Scanner;
+
+public class PrimeNum {
+
+	public static void main(String[] args) {
+		System.out.println("Plese Enter A Number");
+		Scanner scanner = new Scanner(System.in);
+		int int1 = scanner.nextInt();
+		int count = 0;
+		for(int i=int1;i>0;i--) {
+			if(int1 % i==0) {
+				count++;
+			}
+		}
+		if(count>2) {
+			System.out.println("Number is not Prime");
+		}
+		else {
+			System.out.println("Number is Prime");
+		}
+	}
+
+}
